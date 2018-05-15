b0b0036b5e11e88c9301162acef746d394706777
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-05-15 10:34:59 +0300

    storefront integration tests, refactor, cov improvements

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import {GatewayBFF} from "./gateway";
 import {Storefront} from "./storefront";
 import {logger} from "./logger";
-
+import * as ENUMS from "./enums";
 
 export = {
     Gateway: GatewayBFF,
     Storefront,
-    logger
+    logger,
+    ENUMS
 };
