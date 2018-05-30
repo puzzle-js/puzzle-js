0c7788aae080701a0a20f34780c0e3bfda6bfa97
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-05-30 12:06:14 +0300

    System tests added. Content headers, status code reflections fixed

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,25 @@
-export const DEFAULT_POLLING_INTERVAL = 1250;
-export const CONTENT_NOT_FOUND_ERROR = `<script>console.log('Fragment Part does not exists')</script>`;
-export const DEFAULT_CONTENT_TIMEOUT = 15000;
-export const RENDER_MODE_QUERY_NAME = '__renderMode';
-export const PREVIEW_PARTIAL_QUERY_NAME = '__partial';
-export const API_ROUTE_PREFIX = 'api';
-export const GATEWAY_PREPERATION_CHECK_INTERVAL = 200;
-export const CHEERIO_CONFIGURATION = {
+const loadConfigurationFromEnv = (name: string, isObject: boolean = false) => {
+    const environmentVariable = process.env[name];
+    if (isObject) {
+        return environmentVariable ? JSON.parse(environmentVariable) : undefined;
+    } else {
+        return environmentVariable;
+    }
+};
+
+
+export const DEFAULT_POLLING_INTERVAL = +loadConfigurationFromEnv('DEFAULT_POLLING_INTERVAL') || 1250;
+export const CONTENT_NOT_FOUND_ERROR = loadConfigurationFromEnv('CONTENT_NOT_FOUND_ERROR') || `<script>console.log('Fragment Part does not exists')</script>`;
+export const DEFAULT_CONTENT_TIMEOUT = +loadConfigurationFromEnv('DEFAULT_CONTENT_TIMEOUT') || 15000;
+export const RENDER_MODE_QUERY_NAME = loadConfigurationFromEnv('RENDER_MODE_QUERY_NAME') || '__renderMode';
+export const PREVIEW_PARTIAL_QUERY_NAME = loadConfigurationFromEnv('PREVIEW_PARTIAL_QUERY_NAME') || '__partial';
+export const API_ROUTE_PREFIX = loadConfigurationFromEnv('API_ROUTE_PREFIX') || 'api';
+export const GATEWAY_PREPERATION_CHECK_INTERVAL = +loadConfigurationFromEnv('GATEWAY_PREPERATION_CHECK_INTERVAL') || 200;
+export const CHEERIO_CONFIGURATION = loadConfigurationFromEnv('CHEERIO_CONFIGURATION', true) || {
     normalizeWhitespace: true,
     recognizeSelfClosing: true,
     xmlMode: true,
     lowerCaseAttributeNames: true,
 };
-export const TEMPLATE_FRAGMENT_TAG_NAME = 'fragment';
-export const DEFAULT_GZIP_EXTENSIONS = ['.js', '.css'];
+export const TEMPLATE_FRAGMENT_TAG_NAME = loadConfigurationFromEnv('TEMPLATE_FRAGMENT_TAG_NAME') || 'fragment';
+export const DEFAULT_GZIP_EXTENSIONS = loadConfigurationFromEnv('DEFAULT_GZIP_EXTENSIONS', true) || ['.js', '.css'];
