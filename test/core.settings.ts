df9612018bd1079c3604186b8d949cb737f28ff4
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-06-20 15:41:05 +0300

    Renamed test certifacate

--- test/core.settings.ts
+++ test/core.settings.ts
@@ -3,7 +3,6 @@ import * as fs from "fs";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-
-export const TLS_KEY = fs.readFileSync(path.join(__dirname, '/keys/server.orig.key'));
-export const TLS_CERT = fs.readFileSync(path.join(__dirname, '/keys/server.orig.crt'));
+export const TLS_KEY = fs.readFileSync(path.join(__dirname, '/keys/server.test.key'));
+export const TLS_CERT = fs.readFileSync(path.join(__dirname, '/keys/server.test.crt'));
 export const TLS_PASS = '123321';
