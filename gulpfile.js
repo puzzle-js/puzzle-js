123722f926f419de1a3776dd2cc4231a5d5d9e0e
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-07-05 14:38:53 +0300

    Lib build task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const replace = require('gulp-replace');
 const package = require('./package');
 
 gulp.task('copyPublic', () => {
-    gulp
-        .src(['src/public/*'])
-        .pipe(replace(`PACKAGE_VERSION = '';`, `PACKAGE_VERSION = '${package.version}';`))
-        .pipe(replace(`const DEPENDENCIES = {};`, `const DEPENDENCIES = ${JSON.stringify(package.dependencies)};`))
-        .pipe(gulp.dest('dist/public'))
+  gulp
+    .src(['src/public/*'])
+    .pipe(replace(`PACKAGE_VERSION = '';`, `PACKAGE_VERSION = '${package.version}';`))
+    .pipe(replace(`const DEPENDENCIES = {};`, `const DEPENDENCIES = ${JSON.stringify(package.dependencies)};`))
+    .pipe(gulp.dest('dist/public'))
 });
 
 gulp.task('replacePuzzleLib', () => {
