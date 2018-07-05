c5f0b666146463adcab3a6dc6ee2d5f6cce03c16
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-07-05 13:03:18 +0300

    Info Module

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -2,12 +2,14 @@ import {PuzzleJs} from "./puzzle";
 import {Core} from "./core";
 import {Info} from "./modules/info";
 import {Variables} from "./modules/variables";
+import {Fragments} from "./modules/fragments";
 
 (function () {
   const MODULES = {
     Core,
     Info,
-    Variables
+    Variables,
+    Fragments
   };
 
   window.PuzzleJs = new PuzzleJs();
