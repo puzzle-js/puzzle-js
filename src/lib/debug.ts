4055321c36547ed1543ba894c7c614bb1c7debc3
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-07-11 20:27:51 +0300

    test

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -17,3 +17,36 @@ import {Fragments} from "./modules/fragments";
 })();
 
 
+
+
+
+/**
+
+ async function getCacheStoragesAssetTotalSize() {
+  // Note: opaque (i.e. cross-domain, without CORS) responses in the cache will return a size of 0.
+  const cacheNames = await caches.keys();
+
+  let total = 0;
+
+  const sizePromises = cacheNames.map(async cacheName => {
+    const cache = await caches.open(cacheName);
+    const keys = await cache.keys();
+    let cacheSize = 0;
+
+    await Promise.all(keys.map(async key => {
+      const response = await cache.match(key);
+      const blob = await response.blob();
+      total += blob.size;
+      cacheSize += blob.size;
+    }));
+
+    console.log(`Cache ${cacheName}: ${cacheSize} bytes`);
+  });
+
+  await Promise.all(sizePromises);
+
+  return `Total Cache Storage: ${total} bytes`;
+}
+
+
+**/
