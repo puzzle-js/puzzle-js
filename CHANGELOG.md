2108d10d88e61a718568d2b46c218b2403a7c850
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-08-03 16:21:58 +0300

    - `originalurl` key added to headers for api requests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-# [3.8.0] - 26-07-2018
+# [3.9.0] - 03-08-2018
+### Added
+- `originalurl` key added to headers for api requests
+
+# [3.8.0] - 03-08-2018
 ### Added
 - `<puzzle-script>` can be used to inline scripts without any parse error
 
--- test/api.spec.ts
+++ test/api.spec.ts
@@ -4,159 +4,203 @@ import {HTTP_METHODS} from "../src/enums";
 import {Server} from "../src/server";
 import supertest from "supertest";
 import {FragmentBFF} from "../src/fragment";
-
+import faker from "faker";
 
 describe('Api', function () {
-    it('should create a new api instance', () => {
-        const api = new Api({
-            liveVersion: '1.0.0',
-            name: 'browsing',
-            testCookie: 'browsing-version',
-            versions: {}
-        });
-
-        expect(api).to.be.instanceOf(Api);
+  it('should create a new api instance', () => {
+    const api = new Api({
+      liveVersion: '1.0.0',
+      name: 'browsing',
+      testCookie: 'browsing-version',
+      versions: {}
     });
 
-    it('should create a new api instance with endpoints', () => {
-        const api = new Api({
-            liveVersion: '1.0.0',
-            name: 'browsing',
-            testCookie: 'browsing-version',
-            versions: {
-                '1.0.0': {
-                    handler: {
-                        'test': (req: any, res: any) => {
-                            res.end('working');
-                        }
-                    },
-                    endpoints: [
-                        {
-                            method: HTTP_METHODS.GET,
-                            path: '/history',
-                            controller: 'test',
-                            middlewares: []
-                        }
-                    ]
-                }
+    expect(api).to.be.instanceOf(Api);
+  });
+
+  it('should create a new api instance with endpoints', () => {
+    const api = new Api({
+      liveVersion: '1.0.0',
+      name: 'browsing',
+      testCookie: 'browsing-version',
+      versions: {
+        '1.0.0': {
+          handler: {
+            'test': (req: any, res: any) => {
+              res.end('working');
             }
-        });
-
-        expect(api).to.be.instanceOf(Api);
-    });
-
-    it('should respond with endpoint handlers', (done) => {
-        const server = new Server();
-
-        const api = new Api({
-            liveVersion: '1.0.0',
-            name: 'browsing',
-            testCookie: 'browsing-version',
-            versions: {
-                '1.0.0': {
-                    handler: {
-                        'test': (req: any, res: any) => {
-                            res.end('working');
-                        }
-                    },
-                    endpoints: [
-                        {
-                            method: HTTP_METHODS.GET,
-                            path: '/history',
-                            controller: 'test',
-                            middlewares: []
-                        }
-                    ]
-                }
+          },
+          endpoints: [
+            {
+              method: HTTP_METHODS.GET,
+              path: '/history',
+              controller: 'test',
+              middlewares: []
             }
-        });
-
-        api.registerEndpoints(server);
-
-
-        supertest(server.app)
-            .get('/api/browsing/history')
-            .expect(200)
-            .end((err, res) => {
-                expect(res.text).to.eq('working');
-                done();
-            });
+          ]
+        }
+      }
     });
 
-    it('should respond with endpoint handlers', (done) => {
-        const server = new Server();
+    expect(api).to.be.instanceOf(Api);
+  });
 
-        const api = new Api({
-            liveVersion: '1.0.0',
-            name: 'browsing',
-            testCookie: 'browsing-version',
-            versions: {
-                '1.0.0': {
-                    handler: {
-                        'test': (req: any, res: any) => {
-                            res.end('working');
-                        }
-                    },
-                    endpoints: [
-                        {
-                            method: HTTP_METHODS.GET,
-                            path: '/history',
-                            controller: 'test',
-                            middlewares: []
-                        }
-                    ]
-                },
-                '1.0.1': {
-                    handler: {
-                        'test': (req: any, res: any) => {
-                            res.end('working1.0.1');
-                        }
-                    },
-                    endpoints: [
-                        {
-                            method: HTTP_METHODS.GET,
-                            path: '/history',
-                            controller: 'test',
-                            middlewares: []
-                        }
-                    ]
-                }
+  it('should respond with endpoint handlers', (done) => {
+    const server = new Server();
+
+    const api = new Api({
+      liveVersion: '1.0.0',
+      name: 'browsing',
+      testCookie: 'browsing-version',
+      versions: {
+        '1.0.0': {
+          handler: {
+            'test': (req: any, res: any) => {
+              res.end('working');
             }
-        });
-
-        api.registerEndpoints(server);
-
-
-        supertest(server.app)
-            .get('/api/browsing/history')
-            .set('Cookie', `browsing-version=1.0.1`)
-            .expect(200)
-            .end((err, res) => {
-                expect(res.text).to.eq('working1.0.1');
-                done();
-            });
+          },
+          endpoints: [
+            {
+              method: HTTP_METHODS.GET,
+              path: '/history',
+              controller: 'test',
+              middlewares: []
+            }
+          ]
+        }
+      }
     });
 
-    it('should resolve handler when not provided', () => {
-        expect(() => {
-            const api = new Api({
-                liveVersion: '1.0.0',
-                name: 'browsing',
-                testCookie: 'browsing-version',
-                versions: {
-                    '1.0.0': {
-                        endpoints: [
-                            {
-                                method: HTTP_METHODS.GET,
-                                path: '/history',
-                                controller: 'test',
-                                middlewares: []
-                            }
-                        ]
-                    }
-                }
-            });
-        }).to.throw();
+    api.registerEndpoints(server);
+
+
+    supertest(server.app)
+      .get('/api/browsing/history')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).to.eq('working');
+        done();
+      });
+  });
+
+  it('should respond with endpoint handlers', (done) => {
+    const server = new Server();
+
+    const api = new Api({
+      liveVersion: '1.0.0',
+      name: 'browsing',
+      testCookie: 'browsing-version',
+      versions: {
+        '1.0.0': {
+          handler: {
+            'test': (req: any, res: any) => {
+              res.end('working');
+            }
+          },
+          endpoints: [
+            {
+              method: HTTP_METHODS.GET,
+              path: '/history',
+              controller: 'test',
+              middlewares: []
+            }
+          ]
+        },
+        '1.0.1': {
+          handler: {
+            'test': (req: any, res: any) => {
+              res.end('working1.0.1');
+            }
+          },
+          endpoints: [
+            {
+              method: HTTP_METHODS.GET,
+              path: '/history',
+              controller: 'test',
+              middlewares: []
+            }
+          ]
+        }
+      }
     });
+
+    api.registerEndpoints(server);
+
+
+    supertest(server.app)
+      .get('/api/browsing/history')
+      .set('Cookie', `browsing-version=1.0.1`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).to.eq('working1.0.1');
+        done();
+      });
+  });
+
+  it('should resolve handler when not provided', () => {
+    expect(() => {
+      const api = new Api({
+        liveVersion: '1.0.0',
+        name: 'browsing',
+        testCookie: 'browsing-version',
+        versions: {
+          '1.0.0': {
+            endpoints: [
+              {
+                method: HTTP_METHODS.GET,
+                path: '/history',
+                controller: 'test',
+                middlewares: []
+              }
+            ]
+          }
+        }
+      });
+    }).to.throw();
+  });
+
+  it('should respond with originalurl in headers', (done) => {
+    const server = new Server();
+
+    const handler = {
+      'test': (req: any, res: any) => {
+        expect(req.headers.originalurl).to.eq(`/${firstParam}/${secondParam}`);
+        console.log(req.path);
+        res.end('working');
+        done();
+      }
+    };
+
+    const firstParam = faker.random.word();
+    const secondParam = faker.random.number();
+
+    const api = new Api({
+      liveVersion: '1.0.0',
+      name: 'browsing',
+      testCookie: 'browsing-version',
+      versions: {
+        '1.0.0': {
+          handler,
+          endpoints: [
+            {
+              method: HTTP_METHODS.GET,
+              path: '/*',
+              controller: 'test',
+              middlewares: []
+            }
+          ]
+        }
+      }
+    });
+
+    api.registerEndpoints(server);
+
+
+    supertest(server.app)
+      .get(`/api/browsing/${firstParam}/${secondParam}`)
+      .expect(200)
+      .end(() => {
+      });
+  });
 });
 
