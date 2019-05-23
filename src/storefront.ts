38658c0eaa17c0a8093230f5f8ff14cff86d75c1
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-05-23 11:34:50 +0300

    Page conditions

--- src/storefront.ts
+++ src/storefront.ts
@@ -176,7 +176,7 @@ export class Storefront {
             const targetPage = page.url.toString();
             logger.info(`Adding page ${page.name} route: ${targetPage}`);
             this.server.addRoute(page.url, HTTP_METHODS.GET, (req, res, next) => {
-                if (page.condition ? page.condition(req) : true) {
+                if (typeof page.condition === 'function' ? page.condition(req) : true) {
                     this.pages[targetPage].handle(req, res);
                 } else {
                     next();
