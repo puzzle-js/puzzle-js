3c84f32bc56475b195bb3a3615ff3004dce6a87e
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-07-17 13:10:01 +0300

    handler refactor

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,6 +1,7 @@
 import {performance} from 'perf_hooks';
 import {ERROR_CODES, PuzzleError} from "./errors";
 
+
 export const sealed = (constructor: Function) => {
     Object.seal(constructor);
     Object.seal(constructor.prototype);
@@ -53,3 +54,4 @@ export const benchmark = (enabled: boolean, logger: (input: any) => void) => {
         return newDescriptor;
     };
 };
+
