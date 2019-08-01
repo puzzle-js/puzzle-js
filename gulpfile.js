cecf99afd144a2becbc7315da278c8843a534fdd
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-08-01 09:31:54 +0300

    Yarn -> Npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,35 @@
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 const package = require('./package');
+const {parallel, series, src} = require('gulp');
 
-gulp.task('copyPublic', () => {
-  gulp
-    .src(['src/public/*'])
+const copyPublic = () => {
+  return src(['src/public/*'])
     .pipe(replace(`PACKAGE_VERSION = '';`, `PACKAGE_VERSION = '${package.version}';`))
     .pipe(replace(`const DEPENDENCIES = {};`, `const DEPENDENCIES = ${JSON.stringify(package.dependencies)};`))
     .pipe(gulp.dest('dist/public'))
-});
+};
 
-gulp.task('replacePuzzleDebugLib', () => {
-  gulp
-    .src(['dist/lib/puzzle_debug.min.js'])
+const replacePuzzleDebugLib = () => {
+  return src(['dist/lib/puzzle_debug.min.js'])
     .pipe(replace(`PACKAGE_VERSION=""`, `PACKAGE_VERSION="${package.version}"`))
     .pipe(replace(`DEPENDENCIES={}`, `DEPENDENCIES=${JSON.stringify(package.dependencies)}`))
     .pipe(replace(`LOGO=""`, `LOGO="${package.logo}"`))
     .pipe(gulp.dest('dist/lib'))
-});
+};
 
-gulp.task('replacePuzzleProdLib', () => {
-  gulp
-    .src(['dist/lib/puzzle.min.js'])
+
+const replacePuzzleProdLib = () => {
+  return src(['dist/lib/puzzle.min.js'])
     .pipe(replace(`PACKAGE_VERSION=""`, `PACKAGE_VERSION="${package.version}"`))
     .pipe(gulp.dest('dist/lib'))
-});
+};
 
-gulp.task('prepareLib', ['replacePuzzleDebugLib', 'replacePuzzleProdLib']);
 
-gulp.task('build', ['copyPublic']);
+module.exports = {
+  copyPublic,
+  replacePuzzleDebugLib,
+  replacePuzzleProdLib,
+  prepareLib: parallel(replacePuzzleDebugLib, replacePuzzleProdLib),
+  build: series(copyPublic)
+};
